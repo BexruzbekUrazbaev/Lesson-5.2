bd83034786d5ec614ea70fc1a725dc9d9820efe8
BexruzbekUrazbaev <bexruzbekurazbaev88@gmail.com>
2024-04-07 22:37:34 +0500

    Homework

---
+++ main.js
@@ -0,0 +1,11 @@
+let name = prompt("Ismingizni kiriting");
+let id = prompt("space idingizni kiriting");
+let password = prompt("space passwordingizni kiriting");
+
+let oquvchi = {
+    name: name,
+    id: id,
+    password: password,
+}
+
+console.log(oquvchi);
